b28fd62db93601c65eef627e36b39e16a3feb81f
Naka <desginernakamura@gmail.com>
2020-10-18 23:22:24 -0300

    Update mapa in pageOrphaneges and add new page

--- docs/public/script/pageOrphanages.js
+++ docs/public/script/pageOrphanages.js
@@ -1,9 +1,27 @@
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+//criando o mapa
+const map = L.map('mapid').setView([-27.2216317,-49.6449752,16.25], 15); //O 15 é o zoom da camera no mapa 
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// criando e adicionando camada de tile(ladrilho)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map); //mapa gratuito é aqui
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+//criando icone
+const icon = L.icon({
+    iconURL: "./public/images/mapa-maker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68], 
+    popupAnchor: [170, 2]
+});
+
+// criando popup sobreposição 
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight:240
+}).setContent('Lar das meninas <a href="orphanages.html?id=1" class="choose-orphanage" <img src="./public/images/arrow-white.svg" > </a>')
+
+//criando e adicionando marcação
+L
+.marker([-27.2216317,-49.6449752,16.25], {icon})
+.addTo(map)
+.bindPopup(popup);
